ab21735738dd5aed451e1f04efa16832081d9ee0
dnaspider <dnaspider@live.com>
2018-03-18 05:50:18 -0400

    Revert "🗽"
    This reverts commit 99181664c15923c85a55ed21500c6647f98db52b.

--- PD.vb
+++ PD.vb
@@ -799,7 +799,7 @@
     Dim g_kb_i As Integer = 0 'kb item c
 
     Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
-        If TextBox2.Text = "'" Or TextBox2.Text = "" Then Exit Sub
+        If TextBox2.Text = "'" Then Exit Sub
         If My.Settings.SettingTitleTip = True And ControlBox = True Then Me.Text = My.Settings.SettingTitleText & " > " & TextBox2.Text
 
 
@@ -1415,7 +1415,7 @@ App:
                 PD()
             Case 2 '«code-»
                 g_code = ListBox1.SelectedItem.ToString.Substring(1, ListBox1.SelectedItem.ToString.IndexOf(_p) - 1)
-                Key(Keys.Back, False, (g_code.Replace(My.Settings.SettingInsertSymbol, "").Replace("-", "").Length)) 'auto bs*#
+                Key(Keys.Back, False, (g_code.Replace("į", "").Replace("-", "").Length)) 'auto bs*#
                 PD()
             Case 3 'code | g_length code
                 g_s = Microsoft.VisualBasic.Right(ListBox1.SelectedItem.ToString, ListBox1.SelectedItem.ToString.Length - g_length)
